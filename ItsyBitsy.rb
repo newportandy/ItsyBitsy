12ecc4068eaa237e2c4dec58735289d9012f2500
Andy Newport <newportandy@gmail.com>
2009-12-23 09:15:22 +1300

    initial commit

---
+++ ItsyBitsy.rb
@@ -0,0 +1,82 @@
+require 'rubygems'
+require 'rack'
+
+class ItsyBitsy
+  def self.build &block
+    app = App.new
+    app.instance_eval &block
+    app
+  end
+  
+  class App
+    def initialize
+      @routes = { :get => {}, :post => {}, :delete => {}, :put => {} }
+      @middleware = []
+    end
+    
+    def method_missing method, *args, &block
+      [:get, :put, :delete, :post].include?(method) ? add_route( method, args[0], &block) : super
+    end
+    
+    def add_route method, matcher, &block
+      if matcher.is_a? String
+        path_params = matcher.scan(/:(\w+)/).flatten
+        matcher = Regexp.new(matcher.gsub(/:(\w+)/, '(\w+)')+'$')
+        eval "def matcher.path_params\n#{path_params.inspect}\nend" if path_params.length > 0
+      end
+      @routes[method][matcher] = block
+    end
+    
+    def helper &block
+      instance_eval &block 
+    end
+    
+    def use middlware
+      @middleware.unshift middlware
+    end
+    
+    def redirect path
+      @response.redirect path
+    end
+    
+    def params
+      @request.params
+    end
+    
+    def call env
+      app = lambda { |env|
+        @request = Rack::Request.new env
+        @response = Rack::Response.new
+        method = @request.request_method.downcase.to_sym
+        path = @request.path_info
+        body_proc = find_route path, method
+        if body_proc then
+          @response.body = body_proc.call || ""
+          @response.finish
+        else
+          @response.status = 404
+          @response.body = "I'm sorry we couldn't process your request."
+          @response.finish
+        end
+      }
+      @middleware.each do |middleware|
+          app = middleware.new app
+      end
+      app.call env
+    end
+    
+    def find_route path, method
+      @routes[method].each do |regex, block|
+        match = regex.match path
+        next unless match
+        if regex.respond_to? :path_params
+          regex.path_params.each_with_index do |path_param, index|
+            params[path_param] = match[index+1]
+          end
+        end
+        return block
+      end
+      nil
+    end
+  end
+end
---
+++ ItsyBitsyCouldaTest.rb
@@ -0,0 +1,83 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'itsybitsy'
+require 'itsybitsymiddleware'
+
+class ItsyBitsyCouldaTest < Test::Unit::TestCase  
+  context "An ItsyBitsy instance" do
+    setup do
+      app = ItsyBitsy.build do
+        get( '/' ) { "get" }
+        post( '/' ) { "post" }
+        delete( '/' ) { "delete" }
+        put( '/' ) { "put" }
+        get( /\/regex/ ) { "regex" }
+        get( '/redirect' ) { redirect '/redirected' }
+        get( '/params' ) { params["test_params"] }
+        get( '/route_params/:id') { params["id"] }
+      end
+      @req = Rack::MockRequest.new app
+    end
+    
+    should "respond to HTTP verbs" do
+      assert_equal( "get", @req.get( '/' ).body )
+      assert_equal( "post", @req.post( '/' ).body )
+      assert_equal( "delete", @req.delete( '/' ).body )
+      assert_equal( "put", @req.put( '/' ).body )
+    end
+    
+    should "redirect correctly" do
+      assert_equal( 302, @req.get( '/redirect' ).status )
+      assert_equal( "/redirected", @req.get( '/redirect' ).headers['Location'] )
+    end
+    
+    should "match routes using regular expressions" do
+      assert_equal( "regex", @req.get( '/regex' ).body )
+    end
+    
+    should "responde with 404 when it can't find a route" do
+      assert_equal( 404, @req.get( '/unknown' ).status )
+    end
+    
+    should "have access to rack's params hash" do
+      assert_equal( "test", @req.get( '/params?test_params=test' ).body )
+    end
+    
+    should "use path params" do
+      assert_equal( "5", @req.get( '/route_params/5' ).body )
+    end
+    
+    context "with two applications" do
+      should "have seperate helpers for each" do
+        app1 = ItsyBitsy.build do
+          helper do
+            def hello
+              "hello"
+            end
+          end
+          get( '/' ) { hello }
+        end
+        app2 = ItsyBitsy.build do
+          get( '/' ) { hello }
+        end
+        @req1 = Rack::MockRequest.new app1
+        @req2 = Rack::MockRequest.new app2
+        assert_equal( "hello", @req1.get( '/' ).body )
+        assert_raise NameError do  @req2.get( '/' ) end
+      end
+    end
+    
+    context "that uses middleware" do
+      should "use it in the correct order" do
+        app = ItsyBitsy.build do
+          use Middleware1
+          use Middleware2
+          get( '/' ) { "get" }
+        end
+        @req = Rack::MockRequest.new app
+        assert_equal( "12get21", @req.get( '/' ).body )
+      end
+    end 
+  end
+end
